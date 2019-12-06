bd1389c51745a2baa0781e380734647be7d1cd61
Kristy <kristyhoran15@gmail.com>
2019-12-06 16:31:44 +1100

    testing conda env

--- abritamr/AmrSetup.py
+++ abritamr/AmrSetup.py
@@ -175,7 +175,7 @@ class Setupamr(object):
 
         # singularity = "--use-singularity --singularity-args '--bind /home'" if self.run_singulairty else ""
         conda = f"--use-conda --conda-prefix {self.conda_path}"
-        cmd = f"snakemake -s Snakefile_abritamr -j {self.jobs} {singularity} 2>&1 | tee -a job.log"
+        cmd = f"snakemake -s Snakefile_abritamr -j {self.jobs} {conda} 2>&1 | tee -a job.log"
         logging.info(f"Running pipeline using command {cmd}. This may take some time.")
         wkfl = subprocess.run(cmd, shell=True, capture_output=True)
         
