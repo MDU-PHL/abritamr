d3bcd79551222aac34eb94d5a0236563993212f4
Kristy <kristyhoran15@gmail.com>
2022-06-04 07:37:04 +1000

    version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="abritamr",
-    version="1.0.7",
+    version="1.0.8",
     description="Running AMRFinderPlus for MDU",
     long_description=long_description,
     long_description_content_type="text/markdown",
