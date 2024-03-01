ffa854e6a7d53b714cfc298a939c19b9d57a5b2d
Kristy <kristyhoran15@gmail.com>
2024-03-01 16:04:32 +1100

    fixing install

--- setup.py
+++ setup.py
@@ -53,5 +53,5 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
-    package_data={"abritamr": ["db/amrfinderplus/data/2024-01-31.1/*", "species_config.json"]}
+    package_data={"abritamr": ["db/amrfinderplus/data/2024-01-31.1/*", "species_config.json","db/"]}
 )
